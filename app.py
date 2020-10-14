bfca36bfb13851da6959dafd622b4a262c624540
jinhyupkim <jinhyup.kim@cj.net>
2020-10-14 17:20:31 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from datetime import datetime
+import re
+import os
+import platform
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    content = "<br>[OS] " + platform.platform() + "<br> [Time] " + datetime.now().strftime("%A, %d %B, %Y at %X")
+    content = "[TEXT] " + "Version 1" + content
+    return content
